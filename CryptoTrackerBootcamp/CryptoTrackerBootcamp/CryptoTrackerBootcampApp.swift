493c73fbc6be3f2943339033a94786bef1cc2692
Mykyta Kuzminov <mykyta.kuzminov26@icloud.com>
2024-03-14 12:48:43 +0200

    Set up a project

---
+++ CryptoTrackerBootcamp/CryptoTrackerBootcamp/CryptoTrackerBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerBootcampApp.swift
+//  CryptoTrackerBootcamp
+//
+//  Created by Mykyta Kuzminov on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
