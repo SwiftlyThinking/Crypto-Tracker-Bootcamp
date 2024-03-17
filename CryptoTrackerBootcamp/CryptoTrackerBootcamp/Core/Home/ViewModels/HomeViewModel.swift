add245fb5fdd968c5623ec2bcfdb8216775ff187
Mykyta Kuzminov <mykyta.kuzminov26@icloud.com>
2024-03-17 13:59:50 +0200

    Download and display live market data

--- CryptoTrackerBootcamp/CryptoTrackerBootcamp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoTrackerBootcamp/CryptoTrackerBootcamp/Core/Home/ViewModels/HomeViewModel.swift
@@ -16,28 +16,29 @@ class HomeViewModel: ObservableObject {
     
     @Published var searchText: String = ""
     
-    private let dataService = CoinDataService()
+    private let coinDataService = CoinDataService()
+    private let marketDataService = MarketDataService()
     
     private var cancellables = Set<AnyCancellable>()
     
     init() {
         addSubscribers()
-        
-        statistics.append(contentsOf: [
-            DeveloperPreview.shared.statistic,
-            DeveloperPreview.shared.statistic,
-            DeveloperPreview.shared.statistic,
-            DeveloperPreview.shared.statistic,
-        ])
     }
     
     func addSubscribers() {
         $searchText
-            .combineLatest(dataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
             .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+        
+        marketDataService.$marketData
+            .map(mapGlobalMarketData)
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
             }
             .store(in: &cancellables)
     }
@@ -51,5 +52,19 @@ class HomeViewModel: ObservableObject {
             coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
         }
+    }
+    
+    private func mapGlobalMarketData(marketDataModel: MarketDataModel?) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+        
+        guard let data = marketDataModel else { return stats }
+        
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        let volume = StatisticModel(title: "24h Volume", value: data.volume)
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        let portfolio = StatisticModel(title: "Porfolio Value", value: "$0.00", percentageChange: 0)
+        
+        stats.append(contentsOf: [marketCap, volume, btcDominance, portfolio])
+        return stats
     }
 }
---
+++ CryptoTrackerBootcamp/CryptoTrackerBootcamp/Services/MarketDataService.swift
@@ -0,0 +1,31 @@
+//
+//  MarketDataService.swift
+//  CryptoTrackerBootcamp
+//
+//  Created by Mykyta Kuzminov on 17.03.2024.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    var marketDataSubscription: AnyCancellable?
+    
+    init() {
+        getData()
+    }
+    
+    private func getData() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global") else { return }
+        
+        marketDataSubscription = NetworkingManager.download(url: url)
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedData) in
+                self?.marketData = returnedData.data
+                self?.marketDataSubscription?.cancel()
+            })
+    }
+}
