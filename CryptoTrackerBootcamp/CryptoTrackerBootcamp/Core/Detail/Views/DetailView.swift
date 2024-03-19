4ff8b4018cf73c6122875e560be4aab2b3f17314
Mykyta Kuzminov <mykyta.kuzminov26@icloud.com>
2024-03-19 17:32:31 +0200

    Create a custom line chart to display historical price data

--- CryptoTrackerBootcamp/CryptoTrackerBootcamp/Core/Detail/Views/DetailView.swift
+++ CryptoTrackerBootcamp/CryptoTrackerBootcamp/Core/Detail/Views/DetailView.swift
@@ -24,23 +24,31 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
-                Text("")
-                    .frame(height: 150)
+            VStack {
+                ChartView(coin: vm.coin)
+                    .padding(.vertical)
                 
-                overviewTitle
-                Divider()
-                overviewGrid
-                
-                additionalTitle
-                Divider()
-                additionalGrid
+                VStack(spacing: 20) {
+                    
+                    overviewTitle
+                    Divider()
+                    overviewGrid
+                    
+                    additionalTitle
+                    Divider()
+                    additionalGrid
+                }
+                .padding()
             }
-            .padding()
         }
         .navigationTitle(vm.coin.name)
         .navigationBarTitleDisplayMode(.large)
         .scrollIndicators(.hidden)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
 }
 
@@ -86,6 +94,17 @@ extension DetailView {
                 ForEach(vm.additionalStatistics) { stat in
                 StatisticView(stat: stat)
             }
+        }
+    }
+    
+    private var navigationBarTrailingItems: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundStyle(Color.secondaryText)
+            
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
         }
     }
 }
